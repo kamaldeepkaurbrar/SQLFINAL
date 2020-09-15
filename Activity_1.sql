3a297e5fe62e51bb863c9a39354bf578858e9c76
kamaldeepkaurbrar <38276093+kamaldeepkaurbrar@users.noreply.github.com>
2020-09-15 13:43:10 +0530

    Add files via upload

---
+++ Activity_1.sql
@@ -0,0 +1,7 @@
+CREATE database Kamal_Activities;
+use Kamal_Activities;
+
+CREATE TABLE salesman (salesman_id int primary key,name varchar(32),city varchar(32),commission int);
+
+DESCRIBE salesman;
+
